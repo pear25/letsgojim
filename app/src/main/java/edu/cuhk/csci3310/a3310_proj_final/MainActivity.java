9e3aec74eaa58fa37e50044aca76cfafaad93a89
Pierson Limas <piersonlimas12@gmail.com>
2023-05-01 00:31:05 +0800

    Initialized Firebase

--- app/src/main/java/edu/cuhk/csci3310/a3310_proj_final/MainActivity.java
+++ app/src/main/java/edu/cuhk/csci3310/a3310_proj_final/MainActivity.java
@@ -2,8 +2,13 @@ package edu.cuhk.csci3310.a3310_proj_final;
 
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -17,9 +22,13 @@ import edu.cuhk.csci3310.a3310_proj_final.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    FirebaseFirestore firestore;
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
@@ -29,6 +38,21 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        firestore = FirebaseFirestore.getInstance();
+        Map<String, Object> users = new HashMap<>();
+        users.put("Test", "Firebase");
+
+        firestore.collection("users").add(users).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Failure", Toast.LENGTH_LONG).show();
+            }
+        });
 
         setSupportActionBar(binding.toolbar);
 
